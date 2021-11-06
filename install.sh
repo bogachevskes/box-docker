e966c8935651433afae62d1953f88d7edf07d15a
powderpower <ace7upp@gmail.com>
2021-11-07 00:49:37 +0300

    Добавил установку docker-compose

--- install.sh
+++ install.sh
@@ -25,6 +25,10 @@ systemctl start docker
 systemctl enable docker
 systemctl status docker
 
+# docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # add vagrant to docker group
 groupadd docker
 usermod -aG docker vagrant
